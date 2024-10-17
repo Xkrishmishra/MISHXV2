85dfeb8be5d3474a17b168befa4502190c3aa6ed
𝐊ʀɪsʜ 𝐌ɪsʜʀᴀ <163736172+Xkrishmishra@users.noreply.github.com>
2024-10-17 13:46:12 +0530

    Update config.py
    Signed-off-by: 𝐊ʀɪsʜ 𝐌ɪsʜʀᴀ  <163736172+Xkrishmishra@users.noreply.github.com>

--- config.py
+++ config.py
@@ -81,18 +81,18 @@ START_IMG_URL = getenv(
     "START_IMG_URL", "https://i.ibb.co/2FSqhSr/photo-2024-09-19-18-33-46-7416425646991081500.jpg"
 )
 PING_IMG_URL = getenv(
-    "PING_IMG_URL", "https://te.legra.ph/file/b8a0c1a00db3e57522b53.jpg"
+    "PING_IMG_URL", "https://envs.sh/Tcy.jpg"
 )
-PLAYLIST_IMG_URL = "https://te.legra.ph/file/4ec5ae4381dffb039b4ef.jpg"
-STATS_IMG_URL = "https://te.legra.ph/file/e906c2def5afe8a9b9120.jpg"
-TELEGRAM_AUDIO_URL = "https://te.legra.ph/file/6298d377ad3eb46711644.jpg"
-TELEGRAM_VIDEO_URL = "https://te.legra.ph/file/6298d377ad3eb46711644.jpg"
-STREAM_IMG_URL = "https://te.legra.ph/file/bd995b032b6bd263e2cc9.jpg"
-SOUNCLOUD_IMG_URL = "https://te.legra.ph/file/bb0ff85f2dd44070ea519.jpg"
-YOUTUBE_IMG_URL = "https://te.legra.ph/file/6298d377ad3eb46711644.jpg"
-SPOTIFY_ARTIST_IMG_URL = "https://te.legra.ph/file/37d163a2f75e0d3b403d6.jpg"
-SPOTIFY_ALBUM_IMG_URL = "https://te.legra.ph/file/b35fd1dfca73b950b1b05.jpg"
-SPOTIFY_PLAYLIST_IMG_URL = "https://te.legra.ph/file/95b3ca7993bbfaf993dcb.jpg"
+PLAYLIST_IMG_URL = "https://envs.sh/Tcy.jpg"
+STATS_IMG_URL = "https://envs.sh/Tcy.jpg"
+TELEGRAM_AUDIO_URL = "https://envs.sh/Tcy.jpg"
+TELEGRAM_VIDEO_URL = "https://envs.sh/Tcy.jpg"
+STREAM_IMG_URL = "https://envs.sh/Tcy.jpg"
+SOUNCLOUD_IMG_URL = "https://envs.sh/Tcy.jpg"
+YOUTUBE_IMG_URL = "https://envs.sh/Tcy.jpg"
+SPOTIFY_ARTIST_IMG_URL = "https://envs.sh/Tcy.jpg"
+SPOTIFY_ALBUM_IMG_URL = "https://envs.sh/Tcy.jpg"
+SPOTIFY_PLAYLIST_IMG_URL = "https://envs.sh/Tcy.jpg"
 
 
 def time_to_seconds(time):
