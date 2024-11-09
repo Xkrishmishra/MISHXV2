607300a31b11498e3d20a61c9679e9bf73faf90d
𝐊ʀɪsʜ 𝐌ɪsʜʀᴀ <163736172+Xkrishmishra@users.noreply.github.com>
2024-11-09 07:14:54 +0530

    𝐓ᴇᴀᴍᴋʀɪsʜ
    Signed-off-by: 𝐊ʀɪsʜ 𝐌ɪsʜʀᴀ  <163736172+Xkrishmishra@users.noreply.github.com>

--- AnonXMusic/utils/inline/play.py
+++ AnonXMusic/utils/inline/play.py
@@ -1,7 +1,6 @@
 import math
-
+from config import SUPPORT_CHAT, OWNER_USERNAME
 from pyrogram.types import InlineKeyboardButton
-
 from AnonXMusic.utils.formatters import time_to_seconds
 
 
@@ -33,25 +32,25 @@ def stream_markup_timer(_, chat_id, played, dur):
     percentage = (played_sec / duration_sec) * 100
     umm = math.floor(percentage)
     if 0 < umm <= 10:
-        bar = "◉—————————"
+        bar = "Ⓝ▰▱▱▱▱▱▱▱▰"
     elif 10 < umm < 20:
-        bar = "—◉————————"
+        bar = "▰Ⓝ▱▱▱▱▱▱▱▰"
     elif 20 <= umm < 30:
-        bar = "——◉———————"
+        bar = "▰▱Ⓝ▱▱▱▱▱▱▰"
     elif 30 <= umm < 40:
-        bar = "———◉——————"
+        bar = "▰▱▱Ⓝ▱▱▱▱▱▰"
     elif 40 <= umm < 50:
-        bar = "————◉—————"
+        bar = "▰▱▱▱Ⓝ▱▱▱▱▰"
     elif 50 <= umm < 60:
-        bar = "—————◉————"
+        bar = "▰▱▱▱▱Ⓝ▱▱▱▰"
     elif 60 <= umm < 70:
-        bar = "——————◉———"
+        bar = "▰▱▱▱▱▱Ⓝ▱▱▰"
     elif 70 <= umm < 80:
-        bar = "———————◉——"
+        bar = "▰▱▱▱▱▱▱Ⓝ▱▰"
     elif 80 <= umm < 95:
-        bar = "————————◉—"
+        bar = "▰▱▱▱▱▱▱▱Ⓝ▰"
     else:
-        bar = "—————————◉"
+        bar = "▰▱▱▱▱▱▱▱▰Ⓝ"
     buttons = [
         [
             InlineKeyboardButton(
@@ -60,12 +59,26 @@ def stream_markup_timer(_, chat_id, played, dur):
             )
         ],
         [
+            InlineKeyboardButton(text="◁", callback_data=f"ADMIN Resume|{chat_id}"),
+            InlineKeyboardButton(text="☊", callback_data=f"ADMIN Pause|{chat_id}"),
+            InlineKeyboardButton(text="▷", callback_data=f"ADMIN Skip|{chat_id}"),
+            InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}"),
+        ],
+        [
             InlineKeyboardButton(
-                text="🥀𝖴𝗉𝖽𝖺𝗍𝖾 ", url=f"https://t.me/krishnetwork"
+
+                text="ᴜᴘᴅᴀᴛᴇ",
+
+                url=f"t.me/Krishnetwork",
+
             ),
             InlineKeyboardButton(
-                text="🥀𝖲𝗎𝗉𝗉𝗈𝗋𝗍 ", url=f"https://t.me/zxchat2"
-          ),
+
+                text="sᴜᴘᴘᴏʀᴛ",
+
+                url=f"t.me/krishsupport",
+
+            ),
         ],
         [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
     ]
@@ -75,12 +88,26 @@ def stream_markup_timer(_, chat_id, played, dur):
 def stream_markup(_, chat_id):
     buttons = [
         [
+            InlineKeyboardButton(text="◁", callback_data=f"ADMIN Resume|{chat_id}"),
+            InlineKeyboardButton(text="☊", callback_data=f"ADMIN Pause|{chat_id}"),
+            InlineKeyboardButton(text="▷", callback_data=f"ADMIN Skip|{chat_id}"),
+            InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}"),
+        ],
+        [
             InlineKeyboardButton(
-                text="🥀𝖴𝗉𝖽𝖺𝗍𝖾𝗌", url=f"https://t.me/krishnetwork"
+
+                text="ᴜᴘᴅᴀᴛᴇ",
+
+                url=f"t.me/krishnetwork",
+
             ),
             InlineKeyboardButton(
-                text="🥀𝖲𝗎𝗉𝗉𝗈𝗋𝗍 ", url=f"https://t.me/zxchat2"
-          ),
+
+                text="sᴜᴘᴘᴏʀᴛ",
+
+                url=f"t.me/krishsupport",
+
+            ),
         ],
         [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
     ]
@@ -156,3 +183,4 @@ def slider_markup(_, videoid, user_id, query, query_type, channel, fplay):
         ],
     ]
     return buttons
+    
